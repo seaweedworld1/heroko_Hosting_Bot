2f3d5ed67e08def56709da429b883da7b3f551b1
seaweedworld1 <89190637+seaweedworld1@users.noreply.github.com>
2021-08-22 09:44:55 +0800

    up

--- discordbot.py
+++ discordbot.py
@@ -16,11 +16,11 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(name="🗼奈何之都1.0   製作:海苔Kevin#4760"))
     print(">> Bot is online 🟢 <<")
 
-@bot.command()
+@client.command()
 async def ping(ctx):
     await ctx.send(f'現在延遲 ➡ {round(bot.latency*1000)}(ms)')
 
-@bot.command()
+@client.command()
 async def em(ctx):
     embed=discord.Embed(title="Discord群組", url="https://discord.gg/CyK6HanMvM", description="⬆找製作者請點上方連結", color=0x09a4f1, timestamp= datetime.datetime.utcnow())
     embed.set_author(name="海苔Kevin#4760", icon_url="https://lh3.googleusercontent.com/krH41yRrH5lOJ4a2mnD4jVUe9HjmEQw5E3RL0tkuv5lyqPnTOOPJ0RMyC5j7ubi4rmU-mocMxvsjxZ1NHRi3YGbpLv-av-IsQjecPr7a5G_NFLjakP6RQMVDwMBB-EgJnLPAus4c9i05Ue0IZMXlpmLRe68aA9BsCiav7i2H8Zre3Y7-8H44uZbJraqASH79wB-6SzGFUb89r-xeBRTIfyIysnTCtwXryOsHG7Q9_kG4zfsFm6N26FCLrIe9hfu6ykro5y-ErhaCXINt3CugVnLiaN2j7X5Ni4jD6j0QS5qA3x6lJ1ziJqVL8kEe8eDrSFoYgzRJ3gl0oVLEuDJiuqegbkZhXSB-Abg_1LeR6YyisFTbBYDFQpobkPhnLs8YAWYdf1ZPmoGg9ibA9-ctPSPdyL394vs766IzCVCX_G22t4UeOdc7_RwSKjo7KJZ3NZHxzMtdGssnpnMY-VJfAmMq3TsazfBed6fYM9W2Pre2bKrF3LssWzNUDkfacxICF504a-me3p2hICfOI9eqraI_H4SURTewQmc1Y0JdsX-ANkVoeOPchdKt0UjC3j820sXEao4CNfUufqEV4wTFkb1s0KVM_BuNrawi3HqemRds6ah5GeDb4-qCPWx7ma76dhbDribO0LjB9neDDJiNIvfaDVVYWZxxIWrcEKrGXLKBYWAaTELomoPTOCUV_fMX16wiPgGby7DDa3kZDwXcxSA=s124-no?authuser=0")
@@ -35,7 +35,7 @@ async def em(ctx):
     embed.set_footer(text="Requisitado per 海苔Kevin#4760")
     await ctx.send(embed=embed)
 
-@bot.command()
+@client.command()
 async def open(ctx):
     await ctx.send('伺服器狀態已更改：🟢')
     activity = discord.Game(name="奈何之都狀態：🟢")
@@ -47,7 +47,7 @@ async def open(ctx):
     await channel.send('點選網址： https://cfx.re/join/m3lzqq')
     await channel.send('@everyone ')
 
-@bot.command()
+@client.command()
 async def ten(ctx):
     await ctx.send('伺服器狀態已更改：十分鐘後重啟🔄')
     activity = discord.Game(name="奈何之都狀態：十分鐘後重啟🔄", type=2)
@@ -56,7 +56,7 @@ async def ten(ctx):
     await channel.send('伺服器狀態：🔄十分鐘後重啟　')
     await channel.send('@everyone ')
 
-@bot.command()
+@client.command()
 async def five(ctx):
     await ctx.send('伺服器狀態已更改：五分鐘後重啟🔄')
     activity = discord.Game(name="奈何之都狀態：五分鐘後重啟🔄", type=2)
@@ -65,7 +65,7 @@ async def five(ctx):
     await channel.send('伺服器狀態：🔄五分鐘後重啟　')
     await channel.send('@everyone ')
 
-@bot.command()
+@client.command()
 async def close(ctx):
     await ctx.send('伺服器狀態已更改：🔴')
     activity = discord.Game(name="奈何之都狀態：🔴", type=2)
