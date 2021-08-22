deaadb7dfea06e9273cef8a410431fd2ed2de804
seaweedworld1 <89190637+seaweedworld1@users.noreply.github.com>
2021-08-22 12:41:02 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -39,8 +39,8 @@ async def em(ctx):
     embed.set_footer(text="Requisitado per 海苔Kevin#4760")
     await ctx.send(embed=embed)
 
-@bot.command(name= 'button')
-async def button(self, ctx):
+@bot.command()
+async def fc(ctx):
     await ctx.channel.send(
         "This is a button test :smile:",
         components=[
