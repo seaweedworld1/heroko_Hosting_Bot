a32f2dc0878d624ca1710ef0cec83808f85ccee9
seaweedworld1 <89190637+seaweedworld1@users.noreply.github.com>
2021-08-22 09:35:48 +0800

    up

--- discordbot.py
+++ discordbot.py
@@ -14,25 +14,62 @@ client = discord.Client()
 async def on_ready():
     print('成功登入')
 
-# 收到訊息時呼叫
-@client.event
-async def on_message(message):
-    # 送信者為Bot時無視
-    if message.author.bot:
-        return
-    
-    if client.user in message.mentions: # @判定
-        translator = googletrans.Translator()
-        robotName = client.user.name
-        first, space, content = message.clean_content.partition('@'+robotName+' ')
-        
-        if content == '':
-            content = first
-        if translator.detect(content).lang == DSTLanguage:
-            return
-        if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
-            remessage = translator.translate(content, dest='zh-tw').text
-            await message.reply(remessage) 
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'現在延遲 ➡ {round(bot.latency*1000)}(ms)')
+
+@bot.command()
+async def em(ctx):
+    embed=discord.Embed(title="Discord群組", url="https://discord.gg/CyK6HanMvM", description="⬆找製作者請點上方連結", color=0x09a4f1, timestamp= datetime.datetime.utcnow())
+    embed.set_author(name="海苔Kevin#4760", icon_url="https://lh3.googleusercontent.com/krH41yRrH5lOJ4a2mnD4jVUe9HjmEQw5E3RL0tkuv5lyqPnTOOPJ0RMyC5j7ubi4rmU-mocMxvsjxZ1NHRi3YGbpLv-av-IsQjecPr7a5G_NFLjakP6RQMVDwMBB-EgJnLPAus4c9i05Ue0IZMXlpmLRe68aA9BsCiav7i2H8Zre3Y7-8H44uZbJraqASH79wB-6SzGFUb89r-xeBRTIfyIysnTCtwXryOsHG7Q9_kG4zfsFm6N26FCLrIe9hfu6ykro5y-ErhaCXINt3CugVnLiaN2j7X5Ni4jD6j0QS5qA3x6lJ1ziJqVL8kEe8eDrSFoYgzRJ3gl0oVLEuDJiuqegbkZhXSB-Abg_1LeR6YyisFTbBYDFQpobkPhnLs8YAWYdf1ZPmoGg9ibA9-ctPSPdyL394vs766IzCVCX_G22t4UeOdc7_RwSKjo7KJZ3NZHxzMtdGssnpnMY-VJfAmMq3TsazfBed6fYM9W2Pre2bKrF3LssWzNUDkfacxICF504a-me3p2hICfOI9eqraI_H4SURTewQmc1Y0JdsX-ANkVoeOPchdKt0UjC3j820sXEao4CNfUufqEV4wTFkb1s0KVM_BuNrawi3HqemRds6ah5GeDb4-qCPWx7ma76dhbDribO0LjB9neDDJiNIvfaDVVYWZxxIWrcEKrGXLKBYWAaTELomoPTOCUV_fMX16wiPgGby7DDa3kZDwXcxSA=s124-no?authuser=0")
+    embed.add_field(name="以下是BOT指令", value="===================", inline=False)
+    embed.add_field(name="[em", value="差看指令or其他 ", inline=True)
+    embed.add_field(name="[ping", value="差看機器人延遲", inline=True)
+    embed.add_field(name="伺服器啟動選項", value="===================", inline=False)
+    embed.add_field(name="[open", value="伺服器開啟", inline=True)
+    embed.add_field(name="[close", value="伺服器關閉", inline=True)
+    embed.add_field(name="[five", value="五分鐘後重啟", inline=True)
+    embed.add_field(name="[ten", value="十分鐘後重啟", inline=True)
+    embed.set_footer(text="Requisitado per 海苔Kevin#4760")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def open(ctx):
+    await ctx.send('伺服器狀態已更改：🟢')
+    activity = discord.Game(name="奈何之都狀態：🟢")
+    await bot.change_presence(status=discord.Status.idle, activity=activity)
+    channel = bot.get_channel(int(jdata['channel_1']))
+    await channel.send('伺服器狀態：🟢已開啟　')
+    await channel.send('－－－－登入方式－－－－')
+    await channel.send('使用F8 connect 1.34.120.212')
+    await channel.send('點選網址： https://cfx.re/join/m3lzqq')
+    await channel.send('@everyone ')
+
+@bot.command()
+async def ten(ctx):
+    await ctx.send('伺服器狀態已更改：十分鐘後重啟🔄')
+    activity = discord.Game(name="奈何之都狀態：十分鐘後重啟🔄", type=2)
+    await bot.change_presence(status=discord.Status.idle, activity=activity)
+    channel = bot.get_channel(int(jdata['channel_1']))
+    await channel.send('伺服器狀態：🔄十分鐘後重啟　')
+    await channel.send('@everyone ')
+
+@bot.command()
+async def five(ctx):
+    await ctx.send('伺服器狀態已更改：五分鐘後重啟🔄')
+    activity = discord.Game(name="奈何之都狀態：五分鐘後重啟🔄", type=2)
+    await bot.change_presence(status=discord.Status.idle, activity=activity)
+    channel = bot.get_channel(int(jdata['channel_1']))
+    await channel.send('伺服器狀態：🔄五分鐘後重啟　')
+    await channel.send('@everyone ')
+
+@bot.command()
+async def close(ctx):
+    await ctx.send('伺服器狀態已更改：🔴')
+    activity = discord.Game(name="奈何之都狀態：🔴", type=2)
+    await bot.change_presence(status=discord.Status.idle, activity=activity)
+    channel = bot.get_channel(int(jdata['channel_1']))
+    await channel.send('伺服器狀態：🔴已關閉　')
 
 # Bot起動
 client.run(TOKEN)
